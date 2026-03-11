522437112a920e6d15b8b6caa804b8982c420c6c
KitchiDioless <dustirris@gmail.com>
2026-03-11 16:21:30 +0300

    fix: fix front

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const httpAdapter = app.getHttpAdapter();
+  const instance = httpAdapter.getInstance();
+  instance.use(
+    express.static(join(__dirname, '..', 'frontend', 'dist')),
+  );
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on port ${port}`);
